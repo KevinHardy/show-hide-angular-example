a80e20ed20ede1903d5ea54f6460ea965746f484
Kevin Hardy <hardy95k@yahoo.com>
2015-02-11 21:07:57 -0700

    updated controller.js index.html

--- controller.js
+++ controller.js
@@ -1,5 +1,14 @@
 var app = angular.module('show-hide');
 
 app.controller('MainController', function($scope) {
-	$scope.showForm = true;
+
+
+	$scope.toggle = function() {
+		if($scope.showForm) {
+			$scope.showForm = false;
+		} else {
+			$scope.showForm = true;
+		}
+	};
+	
 });
